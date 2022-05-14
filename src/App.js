51d42f06a7041b784f27b6add1edc885798f35d8
Chifez <Chifez1@gmail.com>
2022-05-14 23:12:50 +0100

    i finally fixed the toggle

--- src/App.js
+++ src/App.js
@@ -1,40 +1,89 @@
 import './App.css';
-import React,{ useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import Reset from './components/reset';
-import Timer from './components/timer';
+import {Timer1,Timer2} from './components/timer';
 
 
 function App() {
-  const [enable, setEnable] = useState(false);
-  const [bgColor, setBgColor] = useState("gray");
-  const [active,setActive] = useState("1")
-  const [previousclick,setPreviousClick] = useState("2")
+ const [player1,setPlayer1] =useState(false)
+ const [player2,setPlayer2] =useState(false)
+ const [player1Time,setPlayer1Time] =useState(false)
+ const [player2Time,setPlayer2Time] =useState(false)
+ const[player1Color,setPlayer1Color] = useState("gray")
+ const[player2Color,setPlayer2Color] = useState("gray")
+ const [onStart,setOnStart] =useState(false);
+
+useEffect ((e)=>{
+  handleColor(e)
+},[player1,player2])
+
+
+ const startGame =(e) =>{
+   if (e && !onStart){
+     if ((e.target.className === "timer1") && player1 ===false){
+       console.log(e.target.className)
+       setPlayer2(true)
+       setPlayer1(false)
+       setOnStart(true)
+       console.log(player1,player2)
+       return;
+     }
+     else if((e.target.className === "timer2") && player2===false){
+      console.log(e.target.className)
+      setPlayer1(true)
+      setPlayer2(false)
+      setOnStart(true)
+      console.log(player1,player2)
+      return;
+    }
+ }
+}
+
+ const handleToggle = (e) =>{
+   if (onStart){
+     if ((player1 &&  player2 === false) && e.target.className ==="timer1"){
+       setPlayer1(!player1);
+       setPlayer2(!player2);
+       console.log(player1,player2)
+     } else if ((player1 === false &&  player2 === true) && e.target.className ==="timer2"){
+    setPlayer1(!player1);
+    setPlayer2(!player2);
+    console.log(player1,player2)
+  } else{
+  return;
+  }
+ } else{
+   handleColor(e);
+ }
+ console.log(onStart)
+}
+
+const handleColor  =(e) =>{
+  startGame(e);
+  if (player1 && !player2){
+    setPlayer1Color("green")
+    setPlayer2Color("gray")
+  }else if(!player1 && player2){
+    setPlayer2Color("green")
+    setPlayer1Color("gray")
+  }else{
+    return
+  }
+}
+
 
   return (
     <div className="App">
-      <Timer className ="timer1" id="1" player ="player1"
-      bgColor ={!bgColor}
-      setBgColor={setBgColor}
-      enable ={enable}
-      setEnable ={setEnable}
-      active ={active}
-      setActive ={setActive}
-      previousclick={previousclick}
-      setPreviousClick={setPreviousClick}
+      <Timer1 className ="timer1" id="1"
+        player1Color={player1Color}
+        handleToggle ={handleToggle}
       />
       <Reset />
-      <Timer className ="timer2" id="2"  player ="player2"
-      bgColor ={bgColor}
-      setBgColor={setBgColor}
-      enable={enable}
-      setEnable ={setEnable}
-      active ={active}
-      setActive ={setActive}
-      previousclick={previousclick}
-      setPreviousClick={setPreviousClick}
+      <Timer2 className ="timer2" id="2"
+        player2Color={player2Color}
+        handleToggle ={handleToggle}
       />
     </div>
   );
 }
-
 export default App;
--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,64 +1,41 @@
 import React from 'react'
 import {clock} from './clock.js'
 
-const Timer = (props) => {
-    const{ className, player, id, bgColor, setBgColor, enable, setEnable, active, setActive, previousclick,setPreviousClick } = props;
-    const{time} = clock;
-
-
-    const handleToggle = (e) =>{
-      const id = e.target.id
-      if (id === "1" && previousclick === "2"){
-        setActive("2")
-        setPreviousClick("1")
-        // console.log(id)
-      }else if (id ==="2" && previousclick ==="1"){
-        setActive("1")
-        setPreviousClick("2")
-        // console.log(id)
-      }else if((id === "1" && previousclick === "1")||(id ==="2" && previousclick ==="2")){
-        setActive(previousclick)
-        setPreviousClick(previousclick)
-      }
-      console.log(active)
-    }
-    
-    
-    const handleClick = (e) => {
-      handleToggle(e)
-      const color = (enable === true) ? "green" : "gray";
-      // if (id === active){
-        setBgColor(color)
-        setEnable(!enable)
-        //   console.log(enable)
-        //   console.log(id)
-        // } else {
-        //   setBgColor("green")
-        // }
-         }
-      
-    
+export const Timer1 = (props) => {
+  const{className,id,handleToggle,player1Color} =props
+  const {time} =clock
   return (
-      <div>        
+      <div  className={className}
+      onClick={(e)=>handleToggle(e)} style= {{backgroundColor:`${player1Color}`}}
+      >        
         {time.map((timer) => 
-          <div key={id}  id={id} className={className} onClick={(e,id) => {handleClick(e,id)}} 
-          style ={{backgroundColor:`${bgColor}`}} >
-            <h2>{player}</h2>
+          <div key={id}>
             <h3>{timer.min}</h3>
             <h3>:</h3>
             <h3>{timer.sec}</h3>
           </div>
         )}
-
       </div>
-
-
-
     )
   }
 
-  //   <div onClick={(e,className) => {handleClick(e,className)}} style ={{backgroundColor:`${bgColor}`}} >
-   
-  //  </div> 
 
-export default Timer
+  export const Timer2 = (props) => {
+    const{className,id,handleToggle,player2Color} =props
+    const {time} =clock
+    return (
+        <div onClick={(e)=>handleToggle(e)} className={className}
+        style={{backgroundColor:`${player2Color}`}}
+        >        
+          {time.map((timer) => 
+            <div key={id}>
+              <h3>{timer.min}</h3>
+              <h3>:</h3>
+              <h3>{timer.sec}</h3>
+            </div>
+          )}
+  
+        </div>
+      )
+    }
+  
