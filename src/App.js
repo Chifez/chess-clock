89a7fdfe45b02ee765fbe645a90aa861ba39d08e
Chifez <Chifez1@gmail.com>
2022-05-28 15:00:26 +0100

    got the timer still needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{ useEffect, useState } from 'react';
+import React,{ useEffect, useState,useRef } from 'react';
 import Reset from './components/reset';
 import {Timer1,Timer2} from './components/timer';
 
@@ -7,19 +7,25 @@ import {Timer1,Timer2} from './components/timer';
 function App() {
  const [player1,setPlayer1] =useState(false)
  const [player2,setPlayer2] =useState(false)
- const [player1Time,setPlayer1Time] =useState(false)
- const [player2Time,setPlayer2Time] =useState(false)
- const[player1Color,setPlayer1Color] = useState("gray")
- const[player2Color,setPlayer2Color] = useState("gray")
+ const [player1Time,setPlayer1Time] =useState('10:00')
+ const [player2Time,setPlayer2Time] =useState('10:00')
+ const [timeControl,setTimecontrol] = useState(200)
+ const [player1Color,setPlayer1Color] = useState("gray")
+ const [player2Color,setPlayer2Color] = useState("gray")
  const [onStart,setOnStart] =useState(false);
+
 
 useEffect ((e)=>{
   handleColor(e)
+  // handleTime();
 },[player1,player2])
 
+ const player1Ref = useRef(null)
+ const player2Ref = useRef(null)
 
- const startGame =(e) =>{
+ const startGame = (e) =>{
    if (e && !onStart){
+     clearTimer(getDeadline())
      if ((e.target.className === "timer1") && player1 ===false){
        console.log(e.target.className)
        setPlayer2(true)
@@ -70,6 +76,55 @@ const handleColor  =(e) =>{
     return
   }
 }
+const getTime=(endTime)=>{
+  const total = Date.parse(endTime) - Date.parse(new Date())
+  const seconds = Math.floor((total/1000) % 60)
+  const minutes = Math.floor((total/1000 * 60) % 60)
+  const hours = Math.floor((total/1000 * 60 * 60) % 24)
+  const days = Math.floor((total/1000 * 60 * 60 * 24))
+  console.log(total,minutes,seconds,hours,endTime)
+  return{
+    total,seconds,minutes,hours,days
+  }
+}
+const startTime =(deadline)=>{
+  const{total, seconds, minutes, hours, days} = getTime(deadline);
+  if (total >= 0){
+    const timeFormat = (minutes > 9 ? minutes : '0'+minutes) + ':' + (seconds > 9 ? seconds : '0'+seconds)
+    setPlayer1Time(timeFormat)
+    setPlayer2Time(timeFormat)
+  } else{
+    const stop = clearInterval()
+    setPlayer1Time(stop(player1Ref.current))
+    setPlayer2Time(stop(player2Ref.current))
+  }
+  console.log(deadline)
+}
+
+
+
+
+// const handleTime=()=>{
+//  setInterval(()=>{
+//    const time = ((timer) => timer - 1)
+//    setPlayer1Time(time)
+//    setPlayer2Time(time)
+//  },1000)
+// }
+
+const clearTimer=(endTime)=>{
+  const id = setInterval(() =>{
+    startTime(endTime)
+  },1000)
+  player1Ref.current = id;
+  player2Ref.current = id;
+}
+
+const getDeadline=()=>{
+  const deadline= new Date();
+  deadline.setSeconds(deadline.getSeconds()+`${timeControl}`)
+  return deadline;
+}
 
 
   return (
@@ -77,13 +132,18 @@ const handleColor  =(e) =>{
       <Timer1 className ="timer1" id="1"
         player1Color={player1Color}
         handleToggle ={handleToggle}
+        player1Time={player1Time}
+        player1Ref={player1Ref}
       />
       <Reset />
       <Timer2 className ="timer2" id="2"
         player2Color={player2Color}
         handleToggle ={handleToggle}
+        player2Time={player2Time}
+        player2Ref={player2Ref}
       />
     </div>
   );
 }
+
 export default App;
--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,39 +1,31 @@
 import React from 'react'
-import {clock} from './clock.js'
 
 export const Timer1 = (props) => {
-  const{className,id,handleToggle,player1Color} =props
-  const {time} =clock
+  const {className,id,handleToggle,player1Color,player1Ref,player1Time} =props
   return (
-      <div  className={className}
+      <div ref={player1Ref} className={className}
       onClick={(e)=>handleToggle(e)} style= {{backgroundColor:`${player1Color}`}}
       >        
-        {time.map((timer) => 
+      
           <div key={id}>
-            <h3>{timer.min}</h3>
-            <h3>:</h3>
-            <h3>{timer.sec}</h3>
+            <h3>{player1Time}</h3>
           </div>
-        )}
       </div>
     )
   }
 
 
   export const Timer2 = (props) => {
-    const{className,id,handleToggle,player2Color} =props
-    const {time} =clock
+    const {className,id,handleToggle,player2Color,player2Ref,player2Time} =props
     return (
-        <div onClick={(e)=>handleToggle(e)} className={className}
+        <div ref={player2Ref} onClick={(e)=>handleToggle(e)} className={className}
         style={{backgroundColor:`${player2Color}`}}
         >        
-          {time.map((timer) => 
+
             <div key={id}>
-              <h3>{timer.min}</h3>
-              <h3>:</h3>
-              <h3>{timer.sec}</h3>
+              <h3>{player2Time}</h3>
             </div>
-          )}
+
   
         </div>
       )
